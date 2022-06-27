56b8d719e8a4003521eb165c237d62c715bccc90
soju7 <sojucr7@gmail.com>
2022-06-27 22:35:56 +0530

    Calculator basic functionalities added

--- src/Calculator.js
+++ src/Calculator.js
@@ -1,14 +1,41 @@
 
 import './Calculator.css';
 import React, { useEffect, useState } from 'react';
+import { evaluate,format } from 'mathjs'
 
 function Calculator() {
-  const [operationLog,setOperationLog]=useState('12*4-3'); 
-  const [result,setResult]=useState(0); 
+  const [operationLog, setOperationLog] = useState('');
+  const [result, setResult] = useState(0);
+  const SYMBOLS=['*', '-', '/', '+','.'];
 
-  useEffect(() => {
-    setResult(new Function('return ' + operationLog)())
-  },[]);
+  function updateDisplay(value) {
+
+    if (SYMBOLS.includes(value) && SYMBOLS.includes(operationLog[operationLog.length-1])) {
+      return;
+    }
+    let val = operationLog + value;
+    setOperationLog(val)
+    if (!SYMBOLS.includes(value)) {
+      setResult(evaluate(val))
+    }
+  }
+
+  function calculate() {
+    setResult(evaluate(operationLog))
+  }
+
+  function clearAll() {
+    setOperationLog('');
+    setResult(0)
+  }
+
+  function clearLastChar(){
+    let str=operationLog.slice(0, -1)
+    setOperationLog(str);    
+    if (!SYMBOLS.includes(str[str.length-1])) {
+      setResult(evaluate(str))
+    }
+  }
 
   return (
     <div className="calculator">
@@ -17,30 +44,30 @@ function Calculator() {
         <div className='results'>{result}</div>
       </div>
 
-      <button className='button'>C</button>
-      <button className='button'>%</button>
-      <button className='button'>CE</button>
-      <button className='button'>/</button>
+      <button className='button' onClick={() => { clearAll() }}>C</button>
+      <button className='button' onClick={() => { updateDisplay('%') }}>%</button>
+      <button className='button' onClick={() => { clearLastChar() }}>CE</button>
+      <button className='button' onClick={() => { updateDisplay('/') }}>/</button>
 
-      <button className='button' onClick={() => setOperationLog(operationLog + '7')}>7</button>
-      <button className='button' onClick={() => setOperationLog(operationLog + '8')}>8</button>
-      <button className='button' onClick={() => setOperationLog(operationLog + '9')}>9</button>
-      <button className='button' onClick={() => setOperationLog(operationLog + '*')}>*</button>
+      <button className='button' onClick={() => { updateDisplay('7') }}>7</button>
+      <button className='button' onClick={() => { updateDisplay('8') }}>8</button>
+      <button className='button' onClick={() => { updateDisplay('9') }}>9</button>
+      <button className='button' onClick={() => { updateDisplay('*') }}>*</button>
 
-      <button className='button' onClick={() => setOperationLog(operationLog + '4')}>4</button>
-      <button className='button' onClick={() => setOperationLog(operationLog + '5')}>5</button>
-      <button className='button' onClick={() => setOperationLog(operationLog + '6')}>6</button>
-      <button className='button' onClick={() => setOperationLog(operationLog + '-')}>-</button>
+      <button className='button' onClick={() => { updateDisplay('4') }}>4</button>
+      <button className='button' onClick={() => { updateDisplay('5') }}>5</button>
+      <button className='button' onClick={() => { updateDisplay('6') }}>6</button>
+      <button className='button' onClick={() => { updateDisplay('-') }}>-</button>
 
-      <button className='button' onClick={() => setOperationLog(operationLog + '1')}>1</button>
-      <button className='button' onClick={() => setOperationLog(operationLog + '2')}>2</button>
-      <button className='button' onClick={() => setOperationLog(operationLog + '3')}>3</button>
-      <button className='button' onClick={() => setOperationLog(operationLog + '+')}>+</button>
+      <button className='button' onClick={() => { updateDisplay('1') }}>1</button>
+      <button className='button' onClick={() => { updateDisplay('2') }}>2</button>
+      <button className='button' onClick={() => { updateDisplay('3') }}>3</button>
+      <button className='button' onClick={() => { updateDisplay('+') }}>+</button>
 
-      <button className='button' onClick={() => setOperationLog(operationLog + '0')}>0</button>
-      <button className='button'>.</button>
-      <button className='button'>=</button>      
-      <button className='button'>C</button>
+      <button className='button' onClick={() => { updateDisplay('0') }}>0</button>
+      <button className='button' onClick={() => { updateDisplay('.') }}>.</button>
+      <button className='button' onClick={() => { calculate() }}>=</button>
+      <button className='button' onClick={() => { clearAll() }}>C</button>
 
     </div>
   );
